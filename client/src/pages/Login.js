4caa84aa3b993f0ec8e85e4eb313e5114228040f
ido <idoitzhak5@gmail.com>
2023-03-08 13:30:23 +0200

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { FaUser } from 'react-icons/fa';
 
 function Login() {
+
+    //const [email,setEmail] = useState('');
+    const [formData, setFormData] = useState({
+        email: '',
+        passowrd: ''
+    });
+
+    const onChangeText = (e) => {
+        setFormData((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const {email,password} = formData;
+
   return (
     <>  
         <section>
-            <h1 className='headline'>Login To MarketApp</h1>
+            <h1 className='headline'><FaUser/> Login To MarketApp</h1>
+            <p>{email} {password}</p>
 
+        </section>
+
+        <section>
+            <form>
+                <div>
+                    <input 
+                        type='email'
+                        id='email'
+                        name='email'
+                        placeholder='Enter Your Email'
+                        value={email}
+                        onChange={onChangeText}
+                    />        
+                </div>
+
+                <div>
+                    <input 
+                        type='password'
+                        id='password'
+                        name='password'
+                        placeholder='Enter Your Password'
+                        value={password}
+                        onChange={onChangeText}
+                    />        
+                </div>
+            </form>
         </section>
     </>
   )
